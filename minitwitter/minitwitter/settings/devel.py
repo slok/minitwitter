2e3a9a222f0a2a731f5554abbf34df8baf413562
Xabier Larrakoetxea <slok69@gmail.com>
2013-10-14 20:41:53 +0200

    Added devel db settings

--- minitwitter/minitwitter/settings/devel.py
+++ minitwitter/minitwitter/settings/devel.py
@@ -2,3 +2,15 @@ from common import *
 
 DEBUG = True
 SECRET_KEY = "I can haz Keyz"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'minitwitter.db',
+
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
