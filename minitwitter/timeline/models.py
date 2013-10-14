a513d15e8f27640159e177d8ddcea04c1001e7ff
Xabier Larrakoetxea <slok69@gmail.com>
2013-10-14 21:15:04 +0200

    Minor change in model

--- minitwitter/timeline/models.py
+++ minitwitter/timeline/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Message(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    message = models.TextField()
+    data = models.TextField()
     owner = models.ForeignKey('auth.User', related_name='messages')
 
     class Meta:
