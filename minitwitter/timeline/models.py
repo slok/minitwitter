e4900935118c8041036447eda9ce473ab2512411
Xabier Larrakoetxea <slok69@gmail.com>
2013-10-14 21:03:11 +0200

    Added message model

--- minitwitter/timeline/models.py
+++ minitwitter/timeline/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    owner = models.ForeignKey('auth.User', related_name='messages')
+
+    class Meta:
+        ordering = ('created',)
