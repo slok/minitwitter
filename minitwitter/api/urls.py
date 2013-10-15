b1868fd278c1ad5d1271e5140070b3659fc4c7c7
Xabier Larrakoetxea <slok69@gmail.com>
2013-10-15 21:03:45 +0200

    URL changes

--- minitwitter/api/urls.py
+++ minitwitter/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.MessageList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', views.MessageDetail.as_view()),
+    url(r'messages$', views.MessageList.as_view()),
+    url(r'messages/(?P<pk>[0-9]+)/$', views.MessageDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
