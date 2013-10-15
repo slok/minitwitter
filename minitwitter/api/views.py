aaabe1faf6c85fd45c42d9bb7efb2ddd13222156
Xabier Larrakoetxea <slok69@gmail.com>
2013-10-15 19:48:54 +0200

    Added api views

---
+++ minitwitter/api/views.py
@@ -0,0 +1,13 @@
+from timeline.models import Message
+from serializers import MessageSerializer
+from rest_framework import generics
+
+
+class MessageList(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
